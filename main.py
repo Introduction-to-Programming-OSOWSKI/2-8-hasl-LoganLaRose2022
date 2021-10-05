6ad32b5e82c5df2cd4e2a507a4b96f7bfd19016d
LoganLaRose2022 <814107@mystma.org>
2021-10-05 14:48:58 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    for l in range(0, len(w)):
+        if l <= 0:
+            return True
+            return False
+        
+        
+        
+
+print(hasL("dog"))
