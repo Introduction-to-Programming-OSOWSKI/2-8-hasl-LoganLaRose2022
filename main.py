d4393c2969700b2d79fd49352b95f51fe6728daf
LoganLaRose2022 <814107@mystma.org>
2021-10-05 15:08:59 -0500

    3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
 def hasL(w):
     for i in range(0, len(w)):
-        if w[0]== "l":
+        if w[i]== "l":
             return True
             return False
             
@@ -10,4 +10,4 @@ def hasL(w):
         
         
 
-print(hasL("long"))
+print(hasL("little"))
